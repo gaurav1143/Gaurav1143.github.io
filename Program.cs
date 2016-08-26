1494b365f8eb8c9d3ab45d10be51648c2171f0b6
gaurav1143 <gaurav.sde13@gmail.com>
2016-08-26 08:48:14 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MVCCoreDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
+
+
+
+
+
